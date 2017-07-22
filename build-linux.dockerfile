99da70fa15d4eba1a9dc72b0ffed59e11858bd5f
Caleb Doxsey <caleb@doxsey.net>
2017-07-22 09:51:26 -0400

    add arm64

--- build-linux.dockerfile
+++ build-linux.dockerfile
@@ -1,9 +1,21 @@
-FROM alpine:3.6
-
+FROM alpine:3.5 as amd64
 RUN apk update
 RUN apk add gfortran
-
 WORKDIR /go/src/app
-COPY . .
-
+COPY ./src/ ./src/
+COPY ./build.sh ./build.sh
 RUN ./build.sh
+
+
+FROM owlab/alpine-arm64:v3.5 as arm64
+RUN apk update
+RUN apk add gfortran
+WORKDIR /go/src/app
+COPY ./src/ ./src/
+COPY ./build.sh ./build.sh
+RUN ./build.sh
+
+
+FROM alpine:3.5
+COPY --from=amd64 /tmp/quadprog_linux_amd64.syso /tmp/quadprog_linux_amd64.syso
+COPY --from=arm64 /tmp/quadprog_linux_arm64.syso /tmp/quadprog_linux_arm64.syso
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [ "$(uname -m)" == "aarch64" ] ; then
+    ARCH=arm64
+else
+    ARCH=amd64
+fi
 
 ls src -1 \
     | xargs -I {} basename {} .f \
@@ -9,7 +14,7 @@ ls src -1 \
 ls src -1 \
     | xargs -I {} basename {} .f \
     | xargs -I {} echo /tmp/{}.o \
-    | xargs ld -r -o /tmp/quadprog_${OS}_amd64.syso
+    | xargs ld -r -o /tmp/quadprog_${OS}_${ARCH}.syso
 
 ls -alht /tmp
 
@@ -17,5 +22,6 @@ if [ "$OS" == "darwin" ]; then
     docker build . -f build-linux.dockerfile -t badgerodon-quadprog-build
     docker create --name extract badgerodon-quadprog-build:latest
     docker cp extract:/tmp/quadprog_linux_amd64.syso quadprog_linux_amd64.syso
+    docker cp extract:/tmp/quadprog_linux_arm64.syso quadprog_linux_arm64.syso
     docker rm -f extract
 fi
